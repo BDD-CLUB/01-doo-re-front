fe1a053a6379e09a783ed9555b5fb6485cf38e78
gimdogyun <jasper12394@gmail.com>
2024-01-24 21:43:11 +0900

    chore: jotai provider 제거
    #2

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,7 @@
-"use client";
+'use client';
 
-import { ChakraProvider } from "@chakra-ui/react";
-import { Provider } from "jotai";
+import { ChakraProvider } from '@chakra-ui/react';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <Provider>
-      <ChakraProvider>{children}</ChakraProvider>
-    </Provider>
-  );
+  return <ChakraProvider>{children}</ChakraProvider>;
 }
