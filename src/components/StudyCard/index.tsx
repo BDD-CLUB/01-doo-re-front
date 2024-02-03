1b3917295b05df9408e923dbdb0986eb4786886c
munkyeong <horse6953@naver.com>
2024-02-03 14:46:42 +0900

    feat : bookmark 수정 및 이미지 rounded 추가

--- src/components/StudyCard/index.tsx
+++ src/components/StudyCard/index.tsx
@@ -1,14 +1,13 @@
-import { Card, CardBody, CardFooter, Text, Image, Button } from '@chakra-ui/react';
-import { extendTheme } from '@chakra-ui/react';
-import { redirect } from 'next/dist/server/api-utils';
+import { Card, CardBody, CardFooter, Text, Image, Button, Flex } from '@chakra-ui/react';
 
 const StudyCard = () => {
   return (
-    <Card w="52" p="2" rounded="xl" shadow="base" _hover={{ bg: 'gray.100', transition: '0.5s ease-in-out' }}>
+    <Card w="52" p="3" rounded="xl" shadow="base" _hover={{ bg: 'gray.100', transition: '0.5s ease-in-out' }}>
       <Image
         objectFit="cover"
         src="https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
         alt="Chakra UI"
+        rounded="sm"
       />
       <CardBody px="2">
         <Text fontSize="md" fontWeight="bold">
@@ -20,10 +19,12 @@ const StudyCard = () => {
         <Text fontSize="xs" color="gray.400">
           2024/01/01
         </Text>
-        <Button variant="ghost" gap="1">
-          <Image src="\png\bookmark.png" alt="bookmark" w="3" />
+        <Flex alignItems="center">
+          <Button variant="ghost" px="1" minW="3" w="fit-content">
+            <Image src="\png\bookmark.png" alt="bookmark" w="3" />
+          </Button>
           <Text fontSize="xs">1,208</Text>
-        </Button>
+        </Flex>
       </CardFooter>
     </Card>
   );
