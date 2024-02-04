1b83496217f3749219193dfa26c1fa5873dad15e
munkyeong <horse6953@naver.com>
2024-02-04 21:36:22 +0900

    feat : page.tsx 원본 복구

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,12 @@
 'use client';
 
 import { Link } from '@chakra-ui/next-js';
-import { Box } from '@chakra-ui/react';
-import StudyCard from '@/components/StudyCard';
-import { studyCardData } from '@/mocks/study';
 
 const Page = () => {
   return (
-    <Box m="20">
-      <StudyCard
-        name={studyCardData.name}
-        description={studyCardData.description}
-        startDate={studyCardData.startDate}
-        endDate={studyCardData.endDate}
-        cropId={studyCardData.cropId}
-        percent={studyCardData.percent}
-      />
-    </Box>
+    <Link href="/about" color="blue.400" _hover={{ color: 'blue.500' }}>
+      About
+    </Link>
   );
 };
 
