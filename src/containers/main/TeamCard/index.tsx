7604fc678fca850293ca2611477b98528974fc3f
llddang <bodyness@naver.com>
2024-02-15 17:32:38 +0900

    design: 팀 카드 크기 수정
    #32

--- src/containers/main/TeamCard/index.tsx
+++ src/containers/main/TeamCard/index.tsx
@@ -2,7 +2,7 @@ import { Box, Card } from '@chakra-ui/react';
 
 const TeamCard = () => {
   return (
-    <Card overflow="hidden" w={700} h={350} bg="none" borderRadius={30} backdropFilter="blur(30px)">
+    <Card overflow="hidden" w={650} h={350} bg="none" borderRadius={30} backdropFilter="blur(30px)">
       <Box w="100%" h="100%" bg="rgba(255, 255, 255, 0.1)" />
     </Card>
   );
