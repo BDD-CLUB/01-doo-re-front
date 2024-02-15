825fec6b70430ca6815418ddcde78b2ee80e5a7e
llddang <bodyness@naver.com>
2024-02-16 00:44:02 +0900

    design: 메인페이지의 글씨 크기 축소
    #6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,11 +17,11 @@ const Page = () => {
       <Flex key="title_with_login" pos="relative" justify="center" h="100vh" bgColor="green_dark">
         <Grid key="title_with_login" gap="80" templateColumns="2fr 1fr" h="100%">
           <Flex justify="center" direction="column" w="2xl" h="100%" textColor="white" fontWeight="bold">
-            <Text fontSize="150px">DOORE</Text>
-            <Text mb="2" fontSize="50px">
+            <Text fontSize="100px">DOORE</Text>
+            <Text mb="2" fontSize="40px">
               community for developer
             </Text>
-            <Text mb="14" fontSize="30px" fontWeight="semibold">
+            <Text mb="14" fontSize="20px" fontWeight="semibold">
               Investigators have raided the home of the teenage suspect behind the physical attack on ruling party
               lawmaker Bae Hyun-jin as police are trying to determine the exact motive of the...
             </Text>
