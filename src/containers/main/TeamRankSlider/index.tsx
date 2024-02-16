6685c30c55c803ae30bd7bcb24808f4c0e16cb12
llddang <bodyness@naver.com>
2024-02-16 11:12:12 +0900

    feat: 팀 랭킹의 하단 네비게이션 바 구현
    #49

--- src/containers/main/TeamRankSlider/index.tsx
+++ src/containers/main/TeamRankSlider/index.tsx
@@ -6,27 +6,26 @@ import teamRankInfos from '@/mocks/TeamRanking';
 
 const TeamRankSlider = () => {
   const [indexX, setIndexX] = useState<number>(0);
+  const [navigationIdx, setNavigationIdx] = useState<number>(0);
 
   const clickNext = () => {
-    if (indexX < teamRankInfos.length - 1) {
-      setIndexX((prev) => prev + 1);
-    }
+    if (indexX > 5 && navigationIdx < teamRankInfos.length - 13) setNavigationIdx((prev) => prev + 1);
+    if (indexX < teamRankInfos.length - 1) setIndexX((prev) => prev + 1);
   };
 
   const clickPrev = () => {
-    if (indexX > 0) {
-      setIndexX((prev) => prev - 1);
-    }
+    if (indexX < teamRankInfos.length - 7 && navigationIdx > 0) setNavigationIdx((prev) => prev - 1);
+    if (indexX > 0) setIndexX((prev) => prev - 1);
+  };
+
+  const clickNav = (idx: number) => {
+    setIndexX(idx);
+    if (idx > 6 && idx < teamRankInfos.length - 6) setNavigationIdx(idx - 6);
   };
 
   return (
     <Box pos="relative" w="100vw" h="400">
-      <Box
-        className="slider-scene"
-        pos="absolute"
-        left={`calc(50% - ${780 * indexX}px)`}
-        transform="translate(-50%, 0%)"
-      >
+      <Box pos="absolute" left={`calc(50% - ${780 * indexX}px)`} transform="translate(-50%, 0%)">
         {teamRankInfos.map((info, _) => {
           const leftScale = 780 * _;
           return (
@@ -45,6 +44,28 @@ const TeamRankSlider = () => {
         <GridItem onClick={clickPrev} />
         <GridItem onClick={clickNext} />
       </Grid>
+      <Box pos="absolute" top="95%" left="50%" overflow="hidden" w="248px" h="15px" transform="translate(-50%, 0%)">
+        <Box pos="absolute" left={`calc(-${20 * navigationIdx}px)`} w="100%" h="100%">
+          {teamRankInfos.map((info, _) => {
+            const leftScale = 20 * _;
+            return (
+              <Box
+                key={info.id + 100}
+                pos="absolute"
+                left={`calc(${leftScale}px)`}
+                w="2"
+                h="2"
+                bg="#fff"
+                opacity={indexX === _ ? 1 : 0.3}
+                borderRadius="100%"
+                onClick={() => {
+                  clickNav(_);
+                }}
+              />
+            );
+          })}
+        </Box>
+      </Box>
     </Box>
   );
 };
