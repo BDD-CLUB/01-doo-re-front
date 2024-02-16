6824a19561cbd76ae40218d8db6633e423f8a519
llddang <bodyness@naver.com>
2024-02-16 10:21:53 +0900

    feat: 팀 랭킹 페이지에서 다음 거 클릭시 넘어가도록 구현
    #49

--- src/containers/main/TeamRankSlider/index.tsx
+++ src/containers/main/TeamRankSlider/index.tsx
@@ -1,16 +1,36 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Grid, GridItem } from '@chakra-ui/react';
+import { useState } from 'react';
 
 import TeamCard from '@/containers/main/TeamCard';
 import teamRankInfos from '@/mocks/TeamRanking';
 
 const TeamRankSlider = () => {
+  const [indexX, setIndexX] = useState<number>(0);
+
+  const clickNext = () => {
+    if (indexX < teamRankInfos.length - 1) {
+      setIndexX((prev) => prev + 1);
+    }
+  };
+
+  const clickPrev = () => {
+    if (indexX > 0) {
+      setIndexX((prev) => prev - 1);
+    }
+  };
+
   return (
     <Box pos="relative" w="100vw" h="400">
-      <Box className="slider-scene" pos="absolute" left="50%" transform="translate(-50%, 0%)">
+      <Box
+        className="slider-scene"
+        pos="absolute"
+        left={`calc(50% - ${780 * indexX}px)`}
+        transform="translate(-50%, 0%)"
+      >
         {teamRankInfos.map((info, _) => {
           const leftScale = 780 * _;
           return (
-            <Box pos="absolute" left={`calc(50% + ${leftScale}px)`} transform="translate(-50%, 0%)">
+            <Box key={info.id} pos="absolute" left={`calc(50% + ${leftScale}px)`} transform="translate(-50%, 0%)">
               <TeamCard
                 rank={info.rank}
                 name={info.name}
@@ -21,6 +41,10 @@ const TeamRankSlider = () => {
           );
         })}
       </Box>
+      <Grid pos="absolute" columnGap="780" templateColumns="repeat(2, 1fr)" overflow="hidden" w="100%" h="100%">
+        <GridItem onClick={clickPrev} />
+        <GridItem onClick={clickNext} />
+      </Grid>
     </Box>
   );
 };
