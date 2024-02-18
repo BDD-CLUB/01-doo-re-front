3ee8135f62dde8f19d58ae8f36b578d468ca9275
pipisebastian <yrt7998@pusan.ac.kr>
2024-02-18 16:37:19 +0900

    feat: 팀 스터디 파일위치 변경 #60

--- src/app/team/[teamId]/page.tsx
+++ src/app/team/[teamId]/page.tsx
@@ -2,30 +2,33 @@ import { Box, Flex, Grid, GridItem } from '@chakra-ui/react';
 
 const Page = () => {
   return (
-    <Flex gap="8" w="100%" p="8" bg="gray.50">
-      <Flex direction="column" gap="8" w="66%" bg="gray.100">
+    <Flex direction="column" gap="8" w="100%" p="8" bg="gray.50">
+      <Flex justify="space-between" bg="gray.100">
         {/* TODO 이름 + 소개글 컴포넌트 */}
         <Box w="96" h="16" bg="gray.200" />
-        {/* TODO 커리큘럼 */}
-        <Flex flex="1" p="2" bg="gray.200" />
-        {/* TODO 학습자료 */}
-        <Flex direction="column" gap="2">
-          {/* TODO 전체보기 버튼 */}
-          <Box w="100%" h="10" bg="gray.200" />
-          {/* TODO 학습자료 카드 */}
-          <Grid gap="4" templateColumns="repeat(4, 1fr)" bg="gray.100">
-            <GridItem w="100%" h="80" bg="gray.200" />
-            <GridItem w="100%" h="80" bg="gray.200" />
-            <GridItem w="100%" h="80" bg="gray.200" />
-            <GridItem w="100%" h="80" bg="gray.200" />
-          </Grid>
-        </Flex>
+        {/* TODO 팀원 목록, 초대링크 버튼 */}
+        <Box w="52" h="16" bg="gray.200" />
       </Flex>
-      <Flex direction="column" gap="8" w="33%" bg="gray.100">
-        {/* TODO 피드 */}
-        <Flex flex="1" bg="gray.200" />
-        {/* TODO 참여인원 */}
-        <Flex h="300px" bg="gray.200" />
+
+      <Flex align="center" flex="1" gap="8" bg="gray.100">
+        {/* TODO  잔디 */}
+        <Flex flex="1" h="300px" bg="gray.200" />
+        {/* TODO  진행도 */}
+        <Box w="200px" h="200px" bg="gray.200" rounded="full" />
+      </Flex>
+
+      <Flex direction="column" flex="1" gap="4" bg="gray.100">
+        {/* TODO 스터디, 학습자료, 작물창고 버튼 */}
+        <Box w="96" h="10" bg="gray.200" />
+        {/* TODO 전체보기, 네비게이션 이동 버튼 */}
+        <Box h="10" bg="gray.200" />
+        {/* TODO 스터디 카드 */}
+        <Grid gap="4" templateColumns="repeat(4, 1fr)" mx="36" bg="gray.100">
+          <GridItem w="100%" h="64" bg="gray.200" />
+          <GridItem w="100%" h="64" bg="gray.200" />
+          <GridItem w="100%" h="64" bg="gray.200" />
+          <GridItem w="100%" h="64" bg="gray.200" />
+        </Grid>
       </Flex>
     </Flex>
   );
