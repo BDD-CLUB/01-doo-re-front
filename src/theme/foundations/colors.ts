a2838fd2b89db9cd792798a92ff4e8f9a6fe4cc5
Jang seo yun <81643702+pipisebastian@users.noreply.github.com>
2024-02-18 16:15:30 +0900

    design: gray 100, 200 테마 추가 (#66)
    #64

--- src/theme/foundations/colors.ts
+++ src/theme/foundations/colors.ts
@@ -6,6 +6,10 @@ const colors = {
   orange_light: color.orange_light,
   orange: color.orange,
   orange_dark: color.orange_dark,
+  gray: {
+    '100': '#DADADA',
+    '200': '#9A9A9A',
+  },
 };
 
 export default colors;
