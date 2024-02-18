f12e18a41c201b558c623102c2a415d0691d1366
pipisebastian <yrt7998@pusan.ac.kr>
2024-02-18 16:00:42 +0900

    design: 스터디 레이아웃 구현
    #60

--- src/app/team/[teamId]/page.tsx
+++ src/app/team/[teamId]/page.tsx
@@ -1,7 +1,34 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, Flex, Grid, GridItem } from '@chakra-ui/react';
 
 const Page = () => {
-  return <Flex w="100%" bg="#F7F7F7" />;
+  return (
+    <Flex gap="8" w="100%" p="8" bg="#F7F7F7">
+      <Flex direction="column" gap="8" w="66%" bg="gray.100">
+        {/* TODO 이름 + 소개글 컴포넌트 */}
+        <Box w="96" h="16" bg="gray.200" />
+        {/* TODO 커리큘럼 */}
+        <Flex flex="1" p="2" bg="gray.200" />
+        {/* TODO 학습자료 */}
+        <Flex direction="column" gap="2">
+          {/* TODO 전체보기 버튼 */}
+          <Box w="100%" h="10" bg="gray.200" />
+          {/* TODO 학습자료 카드 */}
+          <Grid gap="4" templateColumns="repeat(4, 1fr)" bg="gray.100">
+            <GridItem w="100%" h="80" bg="gray.200" />
+            <GridItem w="100%" h="80" bg="gray.200" />
+            <GridItem w="100%" h="80" bg="gray.200" />
+            <GridItem w="100%" h="80" bg="gray.200" />
+          </Grid>
+        </Flex>
+      </Flex>
+      <Flex direction="column" gap="8" w="33%" bg="gray.100">
+        {/* TODO 피드 */}
+        <Flex flex="1" bg="gray.200" />
+        {/* TODO 참여인원 */}
+        <Flex h="300px" bg="gray.200" />
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Page;
--- src/app/team/[teamId]/study/[studyId]/page.tsx
+++ src/app/team/[teamId]/study/[studyId]/page.tsx
@@ -1,7 +1,37 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, Flex, Grid, GridItem } from '@chakra-ui/react';
 
 const Page = () => {
-  return <Flex w="100%" bg="#F7F7F7" />;
+  return (
+    <Flex direction="column" gap="8" w="100%" p="8" bg="#F7F7F7">
+      <Flex justify="space-between" bg="gray.100">
+        {/* TODO 이름 + 소개글 컴포넌트 */}
+        <Box w="96" h="16" bg="gray.200" />
+        {/* TODO 팀원 목록, 초대링크 버튼 */}
+        <Box w="52" h="16" bg="gray.200" />
+      </Flex>
+
+      <Flex align="center" flex="1" gap="8" bg="gray.100">
+        {/* TODO  잔디 */}
+        <Flex flex="1" h="300px" bg="gray.200" />
+        {/* TODO  진행도 */}
+        <Box w="200px" h="200px" bg="gray.200" rounded="full" />
+      </Flex>
+
+      <Flex direction="column" flex="1" gap="4" bg="gray.100">
+        {/* TODO 스터디, 학습자료, 작물창고 버튼 */}
+        <Box w="96" h="10" bg="gray.200" />
+        {/* TODO 전체보기, 네비게이션 이동 버튼 */}
+        <Box h="10" bg="gray.200" />
+        {/* TODO 스터디 카드 */}
+        <Grid gap="4" templateColumns="repeat(4, 1fr)" mx="36" bg="gray.100">
+          <GridItem w="100%" h="64" bg="gray.200" />
+          <GridItem w="100%" h="64" bg="gray.200" />
+          <GridItem w="100%" h="64" bg="gray.200" />
+          <GridItem w="100%" h="64" bg="gray.200" />
+        </Grid>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Page;
