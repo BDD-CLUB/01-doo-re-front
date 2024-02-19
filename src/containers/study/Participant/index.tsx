d0803e85926e9534c75a81494f91a29ee7888a9f
yeonddori <lsymiso@naver.com>
2024-02-19 18:19:49 +0900

    refactor: Flex 및 Box -> Card로 변경
    #33

--- src/containers/study/Participant/index.tsx
+++ src/containers/study/Participant/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Box, Avatar, Text, Grid, Link } from '@chakra-ui/react';
+import { Flex, Avatar, Text, Grid, Link, Card } from '@chakra-ui/react';
 
 import colors from '@/theme/foundations/colors';
 
@@ -15,25 +15,23 @@ const Participant = ({ participantInfos }: ParticipantProps) => {
   });
 
   return (
-    <Flex justify="center" overflowY="auto" w="30vw" h="30vh" bg="white" borderRadius="2xl" shadow="lg">
-      <Box>
-        <Grid templateColumns="repeat(3, 1fr)" pt="5" pb="2">
-          {[
-            { data: leader, color: colors.orange_dark },
-            ...otherParticipants.map((data) => ({ data, color: 'inherit' })),
-          ].map(({ data, color }) => (
-            <Flex key={data.id} align="center" mr="5" mb="3" ml="5">
-              <Avatar shadow="md" size="xs" src={data.profileImg} />
-              <Link href={data.myPageUrl}>
-                <Text ml="2" color={color} fontSize="sm" fontWeight="bold">
-                  {data.name}
-                </Text>
-              </Link>
-            </Flex>
-          ))}
-        </Grid>
-      </Box>
-    </Flex>
+    <Card align="center" overflowY="auto" w="30vw" h="30vh" m="8" bg="white" borderRadius="2xl" shadow="lg">
+      <Grid templateColumns="repeat(3, 1fr)" pt="5" pb="2">
+        {[
+          { data: leader, color: colors.orange_dark },
+          ...otherParticipants.map((data) => ({ data, color: 'inherit' })),
+        ].map(({ data, color }) => (
+          <Flex key={data.id} mr="5" mb="3" ml="5">
+            <Avatar size="sm" src={data.profileImg} />
+            <Link href={data.myPageUrl}>
+              <Text ml="2" color={color} fontSize="md" fontWeight="bold">
+                {data.name}
+              </Text>
+            </Link>
+          </Flex>
+        ))}
+      </Grid>
+    </Card>
   );
 };
 
