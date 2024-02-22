dc7029dc0a25a461574a7361063077c5b5a914a2
gimdogyun <jasper12394@gmail.com>
2024-02-22 20:30:32 +0900

    feat : dateFormat
    #76

--- src/containers/study/StudyInfoCard/index.tsx
+++ src/containers/study/StudyInfoCard/index.tsx
@@ -2,13 +2,7 @@ import { Card, Flex, Text } from '@chakra-ui/react';
 
 import StudyProgress from '@/containers/study/StudyInfoCard/StudyProgress';
 import { StudyInfoCardProps } from '@/containers/study/StudyInfoCard/types';
-
-const dateFormat = (date: Date) => {
-  const year = String(date.getFullYear()).padStart(4, '0');
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  return `${year}/${month}/${day}`;
-};
+import dateFormat from '@/utils/dateFormat';
 
 const StudyInfoCard = ({ progress, startAt, endAt }: StudyInfoCardProps) => {
   if (progress < 0 || progress > 100) throw new Error('progress는 0~100 사이여야 합니다.');
@@ -20,7 +14,7 @@ const StudyInfoCard = ({ progress, startAt, endAt }: StudyInfoCardProps) => {
         <StudyProgress progress={progress} />
         <Text ml="auto">스터디 기간</Text>
         <Text>
-          {dateFormat(startAt)} - {dateFormat(endAt)}
+          {dateFormat(startAt, '/')} - {dateFormat(endAt, '/')}
         </Text>
       </Flex>
     </Card>
