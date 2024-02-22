247010d8cbdae684a3a17456d7319812468e83c3
llddang <bodyness@naver.com>
2024-02-22 17:39:58 +0900

    feat: Tab Button UI 구현
    #79

--- src/components/TabButton/index.tsx
+++ src/components/TabButton/index.tsx
@@ -4,11 +4,13 @@ import { TabButtonProps } from './type';
 
 const TabButton = ({ currentTab, changeTab, categoryInfos }: TabButtonProps) => {
   return (
-    <Flex>
+    <Flex gap="30px">
       {categoryInfos.map((data) => {
         return (
-          <Button key={data.id}>
-            <Text>{data.name}</Text>
+          <Button key={data.id} w="120px" bg="white" borderRadius="30px" shadow="md">
+            <Text color="black" fontSize="md" fontWeight="bold">
+              {data.name}
+            </Text>
           </Button>
         );
       })}
