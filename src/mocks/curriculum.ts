8510e82413d548eb0e9da49908046c14ca890f30
pipisebastian <yrt7998@pusan.ac.kr>
2024-02-22 20:29:00 +0900

    feat: mocks data추가
    #81

---
+++ src/mocks/curriculum.ts
@@ -0,0 +1,42 @@
+import { CurriculumItemProps } from '@/components/CurriculumCard/types';
+
+const CurriculumCardData: CurriculumItemProps[] = [
+  {
+    id: 1,
+    name: '커리큘럼1',
+    itemOrder: 1,
+    isCompleted: true,
+  },
+  {
+    id: 2,
+    name: '커리큘럼2',
+    itemOrder: 2,
+    isCompleted: false,
+  },
+  {
+    id: 3,
+    name: '커리큘럼3',
+    itemOrder: 3,
+    isCompleted: true,
+  },
+  {
+    id: 4,
+    name: '커리큘럼4',
+    itemOrder: 4,
+    isCompleted: false,
+  },
+  {
+    id: 5,
+    name: '커리큘럼5',
+    itemOrder: 5,
+    isCompleted: false,
+  },
+  {
+    id: 6,
+    name: '커리큘럼6',
+    itemOrder: 6,
+    isCompleted: true,
+  },
+];
+
+export default CurriculumCardData;
