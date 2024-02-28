d4fd2318cd055dd7600dafce1037693d417dc495
llddang <bodyness@naver.com>
2024-02-28 21:10:55 +0900

    chore: 반응형에 따른 font style 지정
    #109

--- src/theme/foundations/index.ts
+++ src/theme/foundations/index.ts
@@ -1,7 +1,9 @@
 import colors from './colors';
+import textStyles from './textStyles';
 
 const foundations = {
   colors,
+  textStyles,
 };
 
 export default foundations;
---
+++ src/theme/foundations/textStyles.ts
@@ -0,0 +1,98 @@
+const textStyles = {
+  bold_4xl: {
+    fontSize: {
+      base: '40px', // 0 ~ 991
+      lg: '44px', // 992 ~ 1535
+      '2xl': '48px', // 1536px ~
+    },
+    fontWeight: 'bold',
+  },
+  bold_3xl: {
+    fontSize: {
+      base: '32px',
+      lg: '36px',
+      '2xl': '40px',
+    },
+    fontWeight: 'bold',
+  },
+  bold_2xl: {
+    fontSize: {
+      base: '22px',
+      lg: '26px',
+      '2xl': '30px',
+    },
+    fontWeight: 'bold',
+  },
+  bold_xl: {
+    fontSize: {
+      base: '16px',
+      lg: '18px',
+      '2xl': '20px',
+    },
+    fontWeight: 'bold',
+  },
+  bold_md: {
+    fontSize: {
+      base: '12px',
+      lg: '14px',
+      '2xl': '16px',
+    },
+    fontWeight: 'bold',
+  },
+  md: {
+    fontSize: {
+      base: '12px',
+      lg: '14px',
+      '2xl': '16px',
+    },
+  },
+  sm: {
+    fontSize: {
+      base: '8px',
+      lg: '10px',
+      '2xl': '12px',
+    },
+  },
+  title_xl: {
+    fontSize: {
+      base: '100px',
+      lg: '120px',
+      '2xl': '150px',
+    },
+    fontWeight: 'black',
+  },
+  title_bold_xl: {
+    fontSize: {
+      base: '100px',
+      lg: '130px',
+      '2xl': '150px',
+    },
+    fontWeight: 'bold',
+  },
+  title_lg: {
+    fontSize: {
+      base: '80px',
+      lg: '92px',
+      '2xl': '100px',
+    },
+    fontWeight: 'black',
+  },
+  title_md: {
+    fontSize: {
+      base: '44px',
+      lg: '48px',
+      '2xl': '52px',
+    },
+    fontWeight: 'bold',
+  },
+  title_sm: {
+    fontSize: {
+      base: '24px',
+      lg: '28px',
+      '2xl': '32px',
+    },
+    fontWeight: 'semibold',
+  },
+};
+
+export default textStyles;
