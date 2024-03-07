8387bd5a58c25ef34c7ac042f6bdab4f75efccb8
llddang <bodyness@naver.com>
2024-03-07 23:47:09 +0900

    refactor: 팀 랭킹 페이지의 페이지 소개글에서 사용되지 않는 css 삭제
    #117

--- src/containers/main/TeamRankDescription/index.tsx
+++ src/containers/main/TeamRankDescription/index.tsx
@@ -4,7 +4,7 @@ const TeamRankDescription = () => {
   return (
     <Flex justify={{ base: 'center', md: 'start' }} w="100%" border="10px solid purple">
       <Flex direction="column" my="2%" ml={{ base: '0%', md: '10%' }} color="white">
-        <Text textStyle="title_lg" alignItems="baseline" display="inline">
+        <Text textStyle="title_lg" display="inline">
           Check
         </Text>
         <Text textStyle="title_lg" display="inline" whiteSpace="nowrap">
