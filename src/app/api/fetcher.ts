c20cf988364a0baf8f1a6ca5d24230e40a754d3b
Kim DoKyun <51306225+jasper200207@users.noreply.github.com>
2024-03-09 11:26:12 +0900

    feat : fetcher 함수 구현 (#120)

---
+++ src/app/api/fetcher.ts
@@ -0,0 +1,54 @@
+type AllowObjectBodyRequestInit = Omit<RequestInit, 'body'> & { body?: RequestInit['body'] | object };
+
+export type FetchProps = [string, AllowObjectBodyRequestInit?];
+
+export type FetcherOptions = {
+  baseUrl?: string;
+  headers?: HeadersInit;
+  interceptors?: {
+    request?: (config: FetchProps) => Promise<FetchProps> | FetchProps;
+    response?: (response: Response) => Promise<Response> | Response;
+  };
+};
+
+const defaultOptions: FetcherOptions = {
+  baseUrl: process.env.NEXT_PUBLIC_API_URL || '',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  interceptors: {
+    request: async (config) => config,
+    response: async (response) => {
+      if (response.ok) {
+        return response;
+      }
+      throw new Error(response.statusText);
+    },
+  },
+};
+
+export const fetcher = (options?: FetcherOptions) => {
+  const { baseUrl, headers, interceptors } = { ...defaultOptions, ...options };
+
+  return async (...props: FetchProps) => {
+    let [url, config] = props;
+    if (interceptors?.request) {
+      [url, config] = await interceptors.request(props);
+    }
+    if (config?.body && typeof config.body === 'object') {
+      config.body = JSON.stringify(config.body);
+    }
+    const fullUrl = url.startsWith('http') ? url : `${baseUrl}${url}`;
+    let response = await fetch(fullUrl, {
+      ...(config as RequestInit),
+      headers: {
+        ...headers,
+        ...(config?.headers || {}),
+      },
+    });
+    if (interceptors?.response) {
+      response = await interceptors.response(response);
+    }
+    return response.json();
+  };
+};
