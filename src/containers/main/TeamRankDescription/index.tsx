af3b8fdcd8411e2af5f53066dbbf7c9c3b5fda10
llddang <bodyness@naver.com>
2024-03-10 20:39:25 +0900

    design: 팀 랭킹 페이지에서 페이지 소개글의 border 삭제
    - 레이아웃 확인하기 위해서 추가했던 border 속성 삭제
    - #117

--- src/containers/main/TeamRankDescription/index.tsx
+++ src/containers/main/TeamRankDescription/index.tsx
@@ -2,7 +2,7 @@ import { Flex, Text } from '@chakra-ui/react';
 
 const TeamRankDescription = () => {
   return (
-    <Flex justify={{ base: 'center', md: 'start' }} w="100%" border="10px solid purple">
+    <Flex justify={{ base: 'center', md: 'start' }} w="100%">
       <Flex direction="column" my="2%" ml={{ base: '0%', md: '10%' }} color="white">
         <Text textStyle="title_lg" display="inline">
           Check
