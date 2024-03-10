8960fa513af3b941fae5fe3ec7c5d415d30b5667
llddang <bodyness@naver.com>
2024-03-10 22:00:19 +0900

    refactor: 사이드바 배경과, 사이드바 통합.
    - #110

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -17,12 +17,17 @@ const Sidebar = () => {
 
   return (
     <>
-      <Box pos="fixed" zIndex="99" h="100%">
+      <Box
+        pos="fixed"
+        zIndex="99"
+        w={!isDesktop && isOpen ? '100%' : '0'}
+        h="100%"
+        bg={!isDesktop && isOpen ? 'rgba(0,0,0,0.5)' : ''}
+      >
         <Box pos="absolute" w={isOpen ? { base: '215px', lg: '230px', '2xl': '240px' } : '52px'}>
           <SidebarContent isOpen={isOpen} setIsOpen={setIsOpen} />
         </Box>
       </Box>
-      <Box pos="fixed" zIndex="2" w={!isDesktop && isOpen ? '100%' : '0'} h="100%" bg="rgba(0,0,0,0.5)" />
       <Box pos="sticky" top="0" left="0">
         <Box w={isDesktop && isOpen ? { base: '215px', lg: '230px', '2xl': '240px' } : '52px'} />
       </Box>
