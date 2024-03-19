31725819078a36a15bc3ba05f294530b54765030
llddang <bodyness@naver.com>
2024-03-19 22:52:47 +0900

    refactor: breakpoint 파일 분리
    - #132

---
+++ src/theme/breakpoints.ts
@@ -0,0 +1,10 @@
+const breakpoints = {
+  base: '0px',
+  sm: '480px',
+  md: '768px',
+  lg: '992px',
+  xl: '1300px',
+  '2xl': '1536px',
+};
+
+export default breakpoints;
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,17 +1,9 @@
 import { extendTheme } from '@chakra-ui/react';
 
+import breakpoints from './breakpoints';
 import components from './components';
 import foundations from './foundations';
 import styles from './styles';
-
-const breakpoints = {
-  base: '0px',
-  sm: '480px',
-  md: '768px',
-  lg: '992px',
-  xl: '1300px',
-  '2xl': '1536px',
-};
 
 const theme = extendTheme({
   breakpoints,
