b0b26b7ac8c7b7376a0eef354d640a9739438658
munkyeong <horse6953@naver.com>
2024-03-20 15:00:34 +0900

    feat : 변수명 변경

--- src/app/api/curriculumItem.ts
+++ src/app/api/curriculumItem.ts
@@ -3,17 +3,17 @@ import { CurriculumItemsDto } from '@/types';
 
 const curriculumFetcher = fetcher();
 
-const postcurriculumFetch = (studyId: number, curriculum: CurriculumItemsDto) => {
+const postCurriculumFetch = (studyId: number, curriculum: CurriculumItemsDto) => {
   curriculumFetcher(`/studies/${studyId}/curriculums`, {
     method: 'POST',
     body: curriculum,
   });
 };
 
-const patchcurriculumFetch = (curriculumId: number, ParticipantId: number) => {
+const patchCurriculumFetch = (curriculumId: number, ParticipantId: number) => {
   curriculumFetcher(`/curriculums/${curriculumId}/${ParticipantId}/check`, {
     method: 'PATCH',
   });
 };
 
-export { postcurriculumFetch, patchcurriculumFetch };
+export { postCurriculumFetch, patchCurriculumFetch };
