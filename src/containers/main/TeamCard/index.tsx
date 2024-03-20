bf4776e3ecdaabe45547576d89868258db6236d9
llddang <bodyness@naver.com>
2024-03-20 19:22:33 +0900

    design: 팀 카드 반응형에 맞게 구현
    - #123

--- src/containers/main/TeamCard/index.tsx
+++ src/containers/main/TeamCard/index.tsx
@@ -1,43 +1,38 @@
-import { Box, Card, CardHeader, Text } from '@chakra-ui/react';
+import { Box, Card, CardHeader, Text, Flex, CardBody, useMediaQuery } from '@chakra-ui/react';
+import { useEffect } from 'react';
 
 import Garden3D from '@/components/Garden3D';
 
 import { TeamCardProps } from './types';
 
-import './style.css';
-
 const TeamCard = ({ rank, name, description, gardenInfos }: TeamCardProps) => {
+  const [isLargerThan992] = useMediaQuery('(min-width: 992px)');
+  const [isLargerThan1536] = useMediaQuery('(min-width: 1536px)');
+
+  useEffect(() => {
+    console.log(isLargerThan992, isLargerThan1536);
+  }, [isLargerThan992, isLargerThan1536]);
+
   return (
-    <Card overflow="hidden" w="650px" h="350px" bg="none" borderRadius="30" backdropFilter="blur(30px)">
-      <Box pos="relative" zIndex="60" w="100%" h="100%" bg="rgba(255, 255, 255, 0.1)">
-        <CardHeader
-          alignItems="center"
-          justifyContent="left"
-          gap="10"
-          display="flex"
-          w="100%"
-          h="120"
-          px="0"
-          pt="5"
-          pb="0"
-          bg="none"
-        >
-          <Box
-            pos="relative"
-            w="18%"
-            h="100%"
-            pr="2"
-            borderBottom="1px"
-            borderBottomStyle="solid"
-            borderBottomColor="white"
-          >
-            <Text color="white" fontFamily="Inter" fontSize="80" fontWeight="700" textAlign="right" fontStyle="normal">
-              {rank}
-            </Text>
+    <Card overflow="hidden" w="100%" h="100%" bg="none" backdropFilter="blur(30px)">
+      <CardHeader
+        gap={{ base: '6', lg: '8', '2xl': '10' }}
+        display="flex"
+        w="100%"
+        h={{ base: '110px', lg: '150px', '2xl': '170px' }}
+        p="0"
+        color="white"
+      >
+        <Box pos="relative" w={{ base: '100px', lg: '120px', '2xl': '170px' }}>
+          <Text textStyle="title_bold_xl" fontFamily="Inter" textAlign="right">
+            {rank}
+          </Text>
+          <Box pos="absolute" top={{ base: '110px', lg: '150px', '2xl': '170px' }} w="100%">
+            <Box className="bar" w="100%" h="2px" bg="white" />
             <Box
               className="circle"
               pos="absolute"
-              top="100%"
+              top="0%"
               left="100%"
               w="2"
               h="2"
@@ -46,36 +41,37 @@ const TeamCard = ({ rank, name, description, gardenInfos }: TeamCardProps) => {
               transform="translate(-50%, -50%)"
             />
           </Box>
-          <Box>
-            <Text color="white" fontFamily="Inter" fontSize="36" fontWeight="700" textAlign="left" fontStyle="normal">
-              {name}
-            </Text>
-            <Text
-              className="team_description"
-              zIndex="20"
-              color="white"
-              fontFamily="Inter"
-              fontSize="16"
-              fontWeight="400"
-              textAlign="left"
-              fontStyle="normal"
-            >
-              {description === '' ? '팀 소개글이 아직 없습니다.' : description}
-            </Text>
-          </Box>
-        </CardHeader>
-        <Box
-          pos="absolute"
-          zIndex="-1"
-          top="20%"
-          left="100%"
-          w="fit-content"
-          h="fit-content"
-          transform="translate(-80%, -10%)"
-        >
-          <Garden3D cubeSize={24} cubeGap={4} rotateY={55} gardenInfos={gardenInfos} />
         </Box>
-      </Box>
+        <Flex direction="column" w="full" pt={{ base: '6', lg: '7', '2xl': '10' }}>
+          <Text textStyle="bold_4xl">{name}</Text>
+          <Text
+            textStyle="md"
+            overflow="hidden"
+            w={{ base: '250px', lg: '300px', '2xl': '350px' }}
+            maxH={{ base: '11', xl: '12' }}
+            whiteSpace="wrap"
+          >
+            {description === '' ? '팀 소개글이 아직 없습니다.' : description}
+          </Text>
+        </Flex>
+      </CardHeader>
+      <CardBody
+        pos="absolute"
+        zIndex="-1"
+        top={{ base: '0%', lg: '10%' }}
+        left={{ base: '28%', lg: '30%', '2xl': '35%' }}
+        w={{ base: '80%', lg: '80%', '2xl': '70%' }}
+        h={{ base: '95%', lg: '90%' }}
+        p="0"
+      >
+        {isLargerThan1536 && <Garden3D cubeSize={32} cubeGap={4} rotateY={55} gardenInfos={gardenInfos} />}
+        {!isLargerThan1536 &&
+          (isLargerThan992 ? (
+            <Garden3D cubeSize={24} cubeGap={4} rotateY={55} gardenInfos={gardenInfos} />
+          ) : (
+            <Garden3D cubeSize={18} cubeGap={4} rotateY={55} gardenInfos={gardenInfos} />
+          ))}
+      </CardBody>
     </Card>
   );
 };
