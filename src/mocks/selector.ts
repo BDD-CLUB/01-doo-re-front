06d288021c621b85aca9e8500f6b244cc798c2ea
yeonddori <lsymiso@naver.com>
2024-03-28 20:24:32 +0900

    chore: mocks 삭제
    #181

--- src/mocks/selector.ts
+++
@@ -1,8 +0,0 @@
-import { SelectorProps } from '@/components/Selector/types';
-
-const selectorData: SelectorProps = {
-  placeholder: '작물을 선택해주세요',
-  label: ['토마토', '고구마', '당근', '완두콩', '벼'],
-};
-
-export default selectorData;
