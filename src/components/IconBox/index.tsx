9ac5b15705a77e0654956be09247384bb3b16407
llddang <77055208+llddang@users.noreply.github.com>
2024-04-04 20:01:24 +0900

    Feature/#190 첨부 파일 관련 공통 컴포넌트 수정 (#191)
    * desing: IconBox  컴포넌트의 radius 수정
    
    - xl -> 2xl
    #190
    
    * design: IconBox의 Icon size 없애고 기본 사이즈 따라가기
    
    #190
    
    * design: 글 사이즈 bold_xl -> bold_md
    
    #190
    
    * design: IconBox padding 없애고 gap 줄이기
    
    #190

--- src/components/IconBox/index.tsx
+++ src/components/IconBox/index.tsx
@@ -4,23 +4,14 @@ import { IconBoxProps } from './type';
 
 const IconBox = ({ leftIcon, content, rightIcon, handleClick }: IconBoxProps) => {
   return (
-    <Flex align="center" gap="2" w="100%" px="3" py="1" color="white" bg="orange_light" borderRadius="xl">
-      <IconButton
-        as="div"
-        flexShrink="0"
-        fontSize="28px"
-        aria-label=""
-        icon={leftIcon}
-        size="icon_md"
-        variant="transparent"
-      />
-      <Text textStyle="bold_xl" flex="auto" isTruncated>
+    <Flex align="center" gap="1" w="100%" color="white" bg="orange_light" borderRadius="2xl">
+      <IconButton as="div" flexShrink="0" aria-label="" icon={leftIcon} size="icon_md" variant="transparent" />
+      <Text textStyle="bold_md" flex="auto" isTruncated>
         {content}
       </Text>
       {rightIcon && (
         <IconButton
           flexShrink="0"
-          fontSize="28px"
           aria-label=""
           icon={rightIcon}
           onClick={handleClick}
