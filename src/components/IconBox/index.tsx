22bd5c5c1c76da7c368da0b23625de6fa421d031
llddang <bodyness@naver.com>
2024-04-04 19:57:00 +0900

    design: IconBox padding 없애고 gap 줄이기
    #190

--- src/components/IconBox/index.tsx
+++ src/components/IconBox/index.tsx
@@ -4,7 +4,7 @@ import { IconBoxProps } from './type';
 
 const IconBox = ({ leftIcon, content, rightIcon, handleClick }: IconBoxProps) => {
   return (
-    <Flex align="center" gap="2" w="100%" px="3" py="1" color="white" bg="orange_light" borderRadius="2xl">
+    <Flex align="center" gap="1" w="100%" color="white" bg="orange_light" borderRadius="2xl">
       <IconButton as="div" flexShrink="0" aria-label="" icon={leftIcon} size="icon_md" variant="transparent" />
       <Text textStyle="bold_md" flex="auto" isTruncated>
         {content}
