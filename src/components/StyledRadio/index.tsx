cb8c98f457d30db1b3ece1f34c94156b353f9f2b
gimdogyun <jasper12394@gmail.com>
2024-04-09 15:28:18 +0900

    design : StyledRadio dot과텍스트 사이거리 1로 수정
    #193

--- src/components/StyledRadio/index.tsx
+++ src/components/StyledRadio/index.tsx
@@ -23,7 +23,7 @@ const StyledRadio = (props: StyledRadioProps) => {
         justify="center"
         w={6}
         h={6}
-        mr={3}
+        mr={1}
         borderWidth="3px"
         borderColor={color.orange_light}
         borderRadius="full"
