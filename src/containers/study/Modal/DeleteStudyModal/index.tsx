1dcdbb16151dff11650edd68b78b429a9784feab
llddang <bodyness@naver.com>
2024-04-11 14:41:35 +0900

    feat: 스터디 삭제 모달 구현
    #201

---
+++ src/containers/study/Modal/DeleteStudyModal/index.tsx
@@ -0,0 +1,30 @@
+import { Text } from '@chakra-ui/react';
+
+import ConfirmModal from '@/components/Modal/ConfirmModal';
+
+import { DeleteStudyModalProps } from '../types';
+
+const DeleteStudyModal = ({ studyName, isOpen, setIsOpen }: DeleteStudyModalProps) => {
+  const handleClickDelete = () => {
+    // TODO - API 연결
+    setIsOpen(false);
+  };
+
+  return (
+    <ConfirmModal
+      isOpen={isOpen}
+      onClose={() => setIsOpen(false)}
+      title="스터디 삭제"
+      confirmButtonText="삭제"
+      onConfirmButtonClick={handleClickDelete}
+    >
+      <Text align="center">
+        삭제된 스터디는 되돌릴 수 없습니다.
+        <br />
+        &quot;{studyName}&quot;을 삭제하시겠습니까?
+      </Text>
+    </ConfirmModal>
+  );
+};
+
+export default DeleteStudyModal;
