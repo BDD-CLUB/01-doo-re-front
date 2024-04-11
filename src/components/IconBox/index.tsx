61c095c58ff4137608f463000fed73cb6cbbf360
llddang <bodyness@naver.com>
2024-04-11 13:48:37 +0900

    design: IconBox에서 Text 필드 마우스 커서가 default로 유지
    #186

--- src/components/IconBox/index.tsx
+++ src/components/IconBox/index.tsx
@@ -6,7 +6,7 @@ const IconBox = ({ leftIcon, content, rightIcon, handleClick }: IconBoxProps) =>
   return (
     <Flex align="center" gap="1" w="100%" color="white" bg="orange_light" borderRadius="2xl">
       <IconButton as="div" flexShrink="0" aria-label="" icon={leftIcon} size="icon_md" variant="transparent" />
-      <Text textStyle="bold_md" flex="auto" isTruncated>
+      <Text textStyle="bold_md" flex="auto" cursor="default" isTruncated>
         {content}
       </Text>
       {rightIcon && (
