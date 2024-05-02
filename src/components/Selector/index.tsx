f6ffaee47876cd8a56a7607c2b09261706a8ec17
yeonddori <lsymiso@naver.com>
2024-05-02 16:38:04 +0900

    feat: onBlur 속성 추가
    #204

--- src/components/Selector/index.tsx
+++ src/components/Selector/index.tsx
@@ -6,7 +6,7 @@ import { BiChevronDown } from 'react-icons/bi';
 
 import { SelectorProps } from './types';
 
-const Selector = ({ selected, label, handleSelector }: SelectorProps) => {
+const Selector = ({ selected, label, handleSelector, onBlur }: SelectorProps) => {
   const menuRef = useRef<HTMLDivElement>(null);
   const [menuWidth, setMenuWidth] = useState('0px');
 
@@ -17,7 +17,7 @@ const Selector = ({ selected, label, handleSelector }: SelectorProps) => {
   }, []);
 
   return (
-    <Menu>
+    <Menu onClose={onBlur}>
       <MenuButton
         ref={menuRef}
         as={Button}
--- src/components/Selector/types.ts
+++ src/components/Selector/types.ts
@@ -2,4 +2,5 @@ export interface SelectorProps {
   selected: string;
   label: string[];
   handleSelector: (data: string) => void;
+  onBlur?: () => void;
 }
