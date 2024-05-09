9702dd51e462d4ebabeb7e5d6abbf14d67aaa49b
llddang <bodyness@naver.com>
2024-05-09 19:06:23 +0900

    feat: tabButton 밑에 전체보기 button 구현
    #200

--- src/mocks/team.ts
+++ src/mocks/team.ts
@@ -1,9 +1,9 @@
 import { TabButtonInfoType } from '@/types';
 
 const teamPageCategoryInfos: TabButtonInfoType[] = [
-  { id: 1, name: '스터디' },
-  { id: 2, name: '학습자료' },
-  { id: 3, name: '작물창고' },
+  { id: 1, name: '스터디', wholeView: true, page: '/' },
+  { id: 2, name: '학습자료', wholeView: true, page: '/' },
+  { id: 3, name: '작물창고', wholeView: false },
 ];
 
 export default teamPageCategoryInfos;
--- src/theme/components/button.ts
+++ src/theme/components/button.ts
@@ -9,19 +9,41 @@ const Button = defineStyleConfig({
     white: {
       bg: 'white',
       color: 'black',
+      _hover: { bg: 'gray.75' },
       minW: '24',
     },
     orange: {
       bg: 'orange',
       color: 'white',
+      _hover: { bg: 'orange_dark' },
+      minW: '24',
+    },
+    orange_light: {
+      bg: 'orange_light',
+      color: 'white',
+      _hover: { bg: 'orange' },
+      minW: '24',
+    },
+    orange_dark: {
+      bg: 'orange_dark',
+      color: 'white',
+      _hover: { bg: '#FF6347' },
       minW: '24',
     },
     transparent: {
       bg: 'transparent',
       color: 'white',
-      _hover: {
-        bg: 'transparent',
-      },
+      _hover: { bg: 'transparent' },
+    },
+    icon_white: {
+      bg: 'white',
+      color: 'black',
+      _hover: { bg: 'orange_light' },
+    },
+    icon_orange_dark: {
+      bg: 'orange_dark',
+      color: 'white',
+      _hover: { bg: 'orange_light' },
     },
   },
 
