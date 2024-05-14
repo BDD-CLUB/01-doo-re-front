5249ed1f65158b26473d19de2320ca80c07e73f1
Kim DoKyun <51306225+jasper200207@users.noreply.github.com>
2024-05-14 22:33:31 +0900

    Feature/#75 로그인 기능 (#210)
    * feat : 로그인 기능
    
    #75
    
    * fix : fetcher 내부에서 에러 발생 시에도 response return
    
    #75
    
    * fix : redirect 대신 router 사용
    
    #75
    
    * feat : git action에 .env 생성 스크립트 추가
    
    #75
    
    * chore : 테스트용 스크립트 제거
    
    #75

---
+++ src/app/oauth2/code/google/page.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { useSetAtom } from 'jotai';
+import { useRouter } from 'next/navigation';
+
+import { postGoogleLoginFetch } from '@/app/api/login';
+import { userAtom } from '@/atom';
+
+const Page = ({ searchParams }: { searchParams: { code: string } }) => {
+  const { code } = searchParams;
+  const router = useRouter();
+
+  const setUser = useSetAtom(userAtom);
+
+  const login = postGoogleLoginFetch();
+  login(code).then((res) => {
+    if (res?.ok) {
+      setUser({
+        memberId: res.body?.memberId,
+        token: res.body?.token,
+        isLogin: true,
+      });
+    } else {
+      alert(res.body.message);
+    }
+    router.replace('/');
+  });
+
+  return <div />;
+};
+
+export default Page;
