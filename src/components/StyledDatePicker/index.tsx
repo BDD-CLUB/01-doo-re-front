ee4e96afd45c6be475a1ed430c6d75c7f538b700
llddang <bodyness@naver.com>
2024-05-20 21:36:54 +0900

    fix: styledDataPicker의 eslint 에러 수정
    #200

--- src/components/StyledDatePicker/index.tsx
+++ src/components/StyledDatePicker/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { Input, InputGroup, Icon, InputRightElement, Box } from '@chakra-ui/react';
 import DatePicker from 'react-datepicker';
 import { BiCalendar } from 'react-icons/bi';
