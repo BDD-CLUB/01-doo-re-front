7bb80f8da695f7bfeb580fcf915aaae9e9f53946
yeonddori <lsymiso@naver.com>
2024-05-28 00:26:36 +0900

    feat: Selector placeholder 속성 추가
    #204

--- src/components/Selector/index.tsx
+++ src/components/Selector/index.tsx
@@ -6,7 +6,7 @@ import { BiChevronDown } from 'react-icons/bi';
 
 import { SelectorProps } from './types';
 
-const Selector = ({ selected, label, handleSelector, handleClose }: SelectorProps) => {
+const Selector = ({ placeholder, selected, label, handleSelector, handleClose }: SelectorProps) => {
   const menuRef = useRef<HTMLDivElement>(null);
   const [menuWidth, setMenuWidth] = useState('0px');
 
@@ -30,7 +30,7 @@ const Selector = ({ selected, label, handleSelector, handleClose }: SelectorProp
         _focus={{ bg: 'orange_light' }}
         rightIcon={<BiChevronDown size="28px" />}
       >
-        <Text textStyle="bold_md">{selected}</Text>
+        <Text textStyle="bold_md">{selected || placeholder}</Text>
       </MenuButton>
       <MenuList
         overflow="hidden"
--- src/components/Selector/types.ts
+++ src/components/Selector/types.ts
@@ -1,4 +1,5 @@
 export interface SelectorProps {
+  placeholder?: string;
   selected: string;
   label: string[];
   handleSelector: (data: string) => void;
