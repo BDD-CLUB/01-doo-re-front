4bd1d5210a3d6578e43a7101ce5c0fb8d19206f6
llddang <bodyness@naver.com>
2024-06-30 00:48:30 +0900

    feat: 팀장 위임하는 버튼 추가
    #256

--- src/containers/team/teamMember/index.tsx
+++ src/containers/team/teamMember/index.tsx
@@ -2,15 +2,16 @@
 
 import { Avatar, AvatarGroup, Box, Flex, IconButton, useBreakpointValue } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
-import { BiUserX } from 'react-icons/bi';
+import { BiCrown, BiUserX } from 'react-icons/bi';
 
 import ConfirmModal from '@/components/Modal/ConfirmModal';
 import { teamMember } from '@/mocks/teamMember';
 import { Member } from '@/types';
 
 const TeamMember = ({ teamId }: { teamId: number }) => {
+  const [mandateModalOpen, setMandateModalOpen] = useState<boolean>(false);
   const [firedModalOpen, setFiredModalOpen] = useState<boolean>(false);
-  const [firedMember, setFiredMember] = useState<Member>({ id: -1, name: '', imageUrl: '' });
+  const [modalMember, setModalMember] = useState<Member>({ id: -1, name: '', imageUrl: '' });
   const [isHovering, setIsHovering] = useState<boolean>(false);
 
   const handleMouseOver = () => {
@@ -22,19 +23,29 @@ const TeamMember = ({ teamId }: { teamId: number }) => {
   };
 
   const handleFiredButtonClick = (member: Member) => {
-    setFiredMember(member);
+    setModalMember(member);
     setFiredModalOpen(true);
+  };
+
+  const handleMandateButtonClick = (member: Member) => {
+    setModalMember(member);
+    setMandateModalOpen(true);
+  };
+
+  const handleModalCloseClick = () => {
+    setMandateModalOpen(false);
+    setFiredModalOpen(false);
+    setIsHovering(false);
   };
 
   const handleFiredMemberClick = () => {
     // TODO: 팀원 퇴출 api 연결
-    setFiredModalOpen(false);
-    setIsHovering(false);
+    handleModalCloseClick();
   };
 
-  const handleModalCloseClick = () => {
-    setFiredModalOpen(false);
-    setIsHovering(false);
+  const handleMandateMemberClick = () => {
+    // TODO: 팀장 권한 위임 api 연결
+    handleModalCloseClick();
   };
 
   useEffect(() => {
@@ -49,29 +60,41 @@ const TeamMember = ({ teamId }: { teamId: number }) => {
         })}
       </AvatarGroup>
       {isHovering && (
-        <Box pos="absolute" zIndex="40" right="0" w="200px" h="400px">
-          <Box w="100%" h="400px" mt="2" p="4" pr="1" bg="white" borderRadius="xl" shadow="md">
+        <Box pos="absolute" zIndex="40" right="0" w="220px" h="400px">
+          <Box w="100%" h="100%" mt="2" p="4" pr="1" bg="white" borderRadius="xl" shadow="md">
             <Box overflow="scroll" w="100%" h="100%">
               {teamMember.map((member) => {
                 return (
-                  <Flex align="center" justify="space-between" gap="2" p="2" pr="6">
+                  <Flex align="center" justify="space-between" gap="2" p="2">
                     <Box>
                       <Avatar key={member.id} mr="2" name={member.name} size="sm" src={member.imageUrl} />
                       {member.name}
                     </Box>
                     {/* TODO: 팀장만 버튼 보이게 수정 */}
-                    <IconButton
-                      w="fit-content"
-                      fontSize="16px"
-                      aria-label=""
-                      icon={<BiUserX />}
-                      isRound
-                      onClick={() => {
-                        handleFiredButtonClick(member);
-                      }}
-                      size="icon_md"
-                      variant="icon_white"
-                    />
+                    <Box>
+                      <IconButton
+                        fontSize="20px"
+                        aria-label=""
+                        icon={<BiCrown />}
+                        isRound
+                        onClick={() => {
+                          handleMandateButtonClick(member);
+                        }}
+                        size="icon_md"
+                        variant="icon_orange"
+                      />
+                      <IconButton
+                        fontSize="20px"
+                        aria-label=""
+                        icon={<BiUserX />}
+                        isRound
+                        onClick={() => {
+                          handleFiredButtonClick(member);
+                        }}
+                        size="icon_md"
+                        variant="icon_white"
+                      />
+                    </Box>
                   </Flex>
                 );
               })}
@@ -86,7 +109,16 @@ const TeamMember = ({ teamId }: { teamId: number }) => {
         confirmButtonText="퇴출"
         onConfirmButtonClick={handleFiredMemberClick}
       >
-        정말 {firedMember.name}을 퇴출하시겠습니까?
+        {modalMember.name}을 퇴출하시겠습니까?
+      </ConfirmModal>
+      <ConfirmModal
+        isOpen={mandateModalOpen}
+        onClose={handleModalCloseClick}
+        title="팀장 위임"
+        confirmButtonText="위임"
+        onConfirmButtonClick={handleMandateMemberClick}
+      >
+        {modalMember.name}에게 팀장을 위임하겠습니까?
       </ConfirmModal>
     </Box>
   );
