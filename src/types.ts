6cd30382d3373b00a3ba3303d8bd3fe71b73150b
llddang <bodyness@naver.com>
2024-06-30 00:01:50 +0900

    feat: Member 타입 정의
    #356

--- src/types.ts
+++ src/types.ts
@@ -71,6 +71,12 @@ export interface TeamDetail extends Team {
   attendanceRate: number;
 }
 
+export interface Member {
+  readonly id: number;
+  name: string;
+  imageUrl: string;
+}
+
 export interface Curriculum {
   id: number;
   name: string;
