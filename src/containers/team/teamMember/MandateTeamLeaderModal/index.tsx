2165d8f44d03f362dafe646b5637d28d1dd01f86
llddang <bodyness@naver.com>
2024-07-17 01:03:34 +0900

    api: 팀장 위임 api 연결
    #287

--- src/containers/team/teamMember/MandateTeamLeaderModal/index.tsx
+++ src/containers/team/teamMember/MandateTeamLeaderModal/index.tsx
@@ -1,12 +1,20 @@
 import { Text } from '@chakra-ui/react';
 
+import { mandateTeamLeader } from '@/app/api/team';
 import ConfirmModal from '@/components/Modal/ConfirmModal';
+import { useMutateWithToken } from '@/hooks/useFetchWithToken';
 
 import { MemberModalProps } from '../types';
 
 const MandateTeamLeaderModal = ({ member, isOpen, teamId, teamName, onClose }: MemberModalProps) => {
-  const handleMandateMemberClick = () => {
-    // TODO: 팀장 권한 위임 api 연결
+  const mandateLeader = useMutateWithToken(mandateTeamLeader);
+
+  const handleMandateLeaderClick = () => {
+    mandateLeader(teamId, member.id).then((res) => {
+      if (!res.ok) {
+        alert('팀장을 위임하는데 실패했습니다.');
+      }
+    });
     onClose();
   };
 
@@ -16,7 +24,7 @@ const MandateTeamLeaderModal = ({ member, isOpen, teamId, teamName, onClose }: M
       onClose={onClose}
       title="팀장 위임"
       confirmButtonText="위임"
-      onConfirmButtonClick={handleMandateMemberClick}
+      onConfirmButtonClick={handleMandateLeaderClick}
     >
       <Text textStyle="bold_md" textAlign="center">
         {`"${member.name}"를 "${teamName}" 팀의 팀장으로`}
