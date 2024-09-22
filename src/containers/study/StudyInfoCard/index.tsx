f5f22ad6463987928e496690a3c0eb10eafd6302
mun-kyeong <horse6953@naver.com>
2024-09-22 20:49:57 +0900

    fix : 스터디 종료 후 상태 프로그래스 바를 "종료된 스터디입니다" 메시지로 변환

--- src/containers/study/StudyInfoCard/index.tsx
+++ src/containers/study/StudyInfoCard/index.tsx
@@ -4,14 +4,22 @@ import StudyProgress from '@/containers/study/StudyInfoCard/StudyProgress';
 import { StudyInfoCardProps } from '@/containers/study/StudyInfoCard/types';
 import dateFormat from '@/utils/dateFormat';
 
-const StudyInfoCard = ({ progress, startAt, endAt }: StudyInfoCardProps) => {
+const StudyInfoCard = ({ progress, startAt, endAt, status }: StudyInfoCardProps) => {
   if (progress < 0 || progress > 100) throw new Error('progress는 0~100 사이여야 합니다.');
 
   return (
     <Card p="4" bg="white" borderRadius="lg">
       <Flex columnGap="4" fontWeight="bold">
-        <Text display={{ base: 'none', '2xl': 'block' }}>스터디 진행률</Text>
-        <StudyProgress progress={progress} />
+        {status === 'ENDED' ? (
+          <Text color="orange"> &quot; 종료된 스터디 입니다. &quot;</Text>
+        ) : (
+          <>
+            {' '}
+            <Text display={{ base: 'none', '2xl': 'block' }}>스터디 진행률</Text>
+            <StudyProgress progress={progress} />
+          </>
+        )}
+
         <Text display={{ base: 'none', '2xl': 'block' }} ml="auto">
           스터디 기간
         </Text>
