abca125502fc9d83f526f680356d706c035eac8e
gimdogyun <jasper12394@gmail.com>
2024-09-27 21:23:30 +0900

    fix: 팀 생성 error시 alert
    - #324

--- src/containers/team/TeamModal/index.tsx
+++ src/containers/team/TeamModal/index.tsx
@@ -105,6 +105,8 @@ const TeamModal = ({ teamInfo, isOpen, onClose }: TeamModalProps) => {
       if (res.ok) {
         refetchSideBar();
         resetAndCloseModal();
+      } else {
+        alert('팀 생성에 실패했습니다.');
       }
     });
   };
