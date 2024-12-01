f390df186082c6c041c0a1118030c57999b83433
llddang <bodyness@naver.com>
2024-12-01 20:01:18 +0900

    feat: 팀의 스터디 갤러리에서 팀원 일때만 스터디 추가 버튼이 보이도록 수정
    #364

--- src/app/team/[teamId]/study-gallery/page.tsx
+++ src/app/team/[teamId]/study-gallery/page.tsx
@@ -1,13 +1,24 @@
 'use client';
 
 import { Button, Flex, Text } from '@chakra-ui/react';
-import { useState } from 'react';
+import { useAtomValue } from 'jotai';
+import { useMemo, useState } from 'react';
 
+import { myTeamAtom } from '@/atom';
 import StudyModal from '@/containers/study/Modal/StudyModal';
 import StudyGallery from '@/containers/study-gallery/StudyGallery';
+import useGetUser from '@/hooks/useGetUser';
 
 const Page = ({ params }: { params: { teamId: number } }) => {
   const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const user = useGetUser();
+  const myTeam = useAtomValue(myTeamAtom);
+
+  const auth = useMemo(() => {
+    if (!user || !user.isLogin) return false;
+    return myTeam.some((teamId) => teamId === +params.teamId);
+  }, [user, myTeam, params.teamId]);
 
   return (
     <Flex align="center" direction="column" gap="9" w="100%" p="8">
@@ -15,9 +26,11 @@ const Page = ({ params }: { params: { teamId: number } }) => {
         <Flex direction="row" gap="2">
           <Text textStyle="bold_2xl">스터디 갤러리</Text>
         </Flex>
-        <Button color="white" bg="orange_dark" onClick={() => setIsOpenModal(true)} rounded="full">
-          스터디 추가
-        </Button>
+        {auth && (
+          <Button color="white" bg="orange_dark" onClick={() => setIsOpenModal(true)} rounded="full">
+            스터디 추가
+          </Button>
+        )}
       </Flex>
       <StudyGallery teamId={params.teamId} refetchTrigger={isOpenModal} />
       <StudyModal teamId={params.teamId} isOpen={isOpenModal} setIsModalOpen={setIsOpenModal} studyInfo={null} />
