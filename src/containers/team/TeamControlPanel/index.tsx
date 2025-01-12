4bee29f81008858700381088a1dbfcbd01629da8
yeonddori <yeonddori@gmail.com>
2025-01-12 19:25:16 +0900

    feat: TeamControlPanel 리더 및 팀원 여부에 따른 버튼 표시
    - 팀원 탈퇴 버튼 추가
    
    #380

--- src/containers/team/TeamControlPanel/index.tsx
+++ src/containers/team/TeamControlPanel/index.tsx
@@ -2,52 +2,83 @@ import { Button, Flex } from '@chakra-ui/react';
 import { useState } from 'react';
 
 import { TeamControlPanelProps } from './types';
-import DeleteTeamModal from '../DeleteTeamModal';
+import TeamActionModal from '../TeamActionModal';
 import TeamModal from '../TeamModal';
 
-const TeamControlPanel = ({ teamInfo }: TeamControlPanelProps) => {
+const TeamControlPanel = ({ isTeamLeader, isMyTeam, teamInfo }: TeamControlPanelProps) => {
   const [isEditModalOpen, setIsEditModalOpen] = useState<boolean>(false);
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState<boolean>(false);
+  const [isLeaveModalOpen, setIsLeaveModalOpen] = useState<boolean>(false);
 
   return (
     <Flex gap="2" mb="8">
-      <Button
-        w="fit-content"
-        px="4"
-        py="1"
-        color="white"
-        bg="orange"
-        shadow="md"
-        _hover={{ bg: 'orange' }}
-        aria-label=""
-        onClick={() => setIsEditModalOpen(true)}
-        size="xs"
-      >
-        수정
-      </Button>
-      <Button
-        w="fit-content"
-        px="4"
-        py="1"
-        color="black"
-        bg="white"
-        shadow="md"
-        _hover={{ bg: 'white' }}
-        aria-label="delete-team-button"
-        onClick={() => setIsDeleteModalOpen(true)}
-        size="xs"
-      >
-        삭제
-      </Button>
+      {isTeamLeader && (
+        <>
+          <Button
+            w="fit-content"
+            px="4"
+            py="1"
+            color="white"
+            bg="orange"
+            shadow="md"
+            _hover={{ bg: 'orange' }}
+            aria-label=""
+            onClick={() => setIsEditModalOpen(true)}
+            size="xs"
+          >
+            수정
+          </Button>
+          <Button
+            w="fit-content"
+            px="4"
+            py="1"
+            color="black"
+            bg="white"
+            shadow="md"
+            _hover={{ bg: 'white' }}
+            aria-label="delete-team-button"
+            onClick={() => setIsDeleteModalOpen(true)}
+            size="xs"
+          >
+            삭제
+          </Button>
+        </>
+      )}
+      {!isTeamLeader && isMyTeam && (
+        <Button
+          w="fit-content"
+          px="4"
+          py="1"
+          color="white"
+          bg="orange"
+          shadow="md"
+          _hover={{ bg: 'orange' }}
+          aria-label="leave-team-button"
+          onClick={() => setIsLeaveModalOpen(true)}
+          size="xs"
+        >
+          탈퇴
+        </Button>
+      )}
       {isEditModalOpen && (
         <TeamModal teamInfo={teamInfo} isOpen={isEditModalOpen} onClose={() => setIsEditModalOpen(false)} />
       )}
       {isDeleteModalOpen && (
-        <DeleteTeamModal
+        <TeamActionModal
           id={teamInfo?.id}
           name={teamInfo?.name}
           isOpen={isDeleteModalOpen}
           onClose={() => setIsDeleteModalOpen(false)}
+          actionType="delete"
+        />
+      )}
+      {isLeaveModalOpen && (
+        <TeamActionModal
+          id={teamInfo?.id}
+          name={teamInfo?.name}
+          isOpen={isLeaveModalOpen}
+          onClose={() => setIsLeaveModalOpen(false)}
+          actionType="leave"
         />
       )}
     </Flex>
