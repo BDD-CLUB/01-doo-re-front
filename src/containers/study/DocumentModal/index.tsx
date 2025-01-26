0098f9cf027382911ef314fff3511e45a37de325
llddang <bodyness@naver.com>
2025-01-26 17:15:32 +0900

    design: Text 요소에서 cursor:text를 cursor:default 로 수정
    #370

--- src/containers/study/DocumentModal/index.tsx
+++ src/containers/study/DocumentModal/index.tsx
@@ -51,20 +51,20 @@ const DocumentModal = ({ id, isOpen, setIsDocsModalOpen, setReload }: DocumentMo
     >
       <Flex textStyle="bold_md" gap="4">
         <Box w={3 / 5} p="4" textColor="white" bgColor={colors.orange_dark} rounded="2xl">
-          <Text>{document?.description}</Text>
+          <Text cursor="default">{document?.description}</Text>
         </Box>
         <Flex justify="space-between" direction="column" w={2 / 5} p="4" bgColor={colors.orange_light} rounded="2xl">
           <Flex justify="space-between">
-            <Text> 작성자</Text>
-            <Text> {document?.uploaderName} </Text>
+            <Text cursor="default"> 작성자</Text>
+            <Text cursor="default"> {document?.uploaderName} </Text>
           </Flex>
           <Flex justify="space-between">
-            <Text> 공개범위</Text>
-            <Text> {document?.accessType === 'ALL' ? '전체 공개' : '팀 공개'} </Text>
+            <Text cursor="default"> 공개범위</Text>
+            <Text cursor="default"> {document?.accessType === 'ALL' ? '전체 공개' : '팀 공개'} </Text>
           </Flex>
         </Flex>
       </Flex>
-      <Text textStyle="bold_xl" mt="8">
+      <Text textStyle="bold_xl" mt="8" cursor="default">
         첨부파일
       </Text>
       <Box className="scroll" overflowY="auto" maxH="72" mt="4">
