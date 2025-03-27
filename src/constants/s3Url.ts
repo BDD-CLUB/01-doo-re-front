4ee2a52103f0d49e67705615cd409b3cb6ffaa79
yeonddori <yeonddori@gmail.com>
2025-03-28 02:14:19 +0900

    chore: S3주소 변경 및 환경변수로 추가
    #410

--- src/constants/s3Url.ts
+++ src/constants/s3Url.ts
@@ -1,3 +1,3 @@
-const S3_URL = (key: string) => `https://doo-re-dev-bucket.s3.ap-northeast-2.amazonaws.com/${key}`;
+const S3_URL = (key: string) => `${process.env.NEXT_PUBLIC_S3_URL}${key}`;
 
 export default S3_URL;
