71804bc1ac6dd74fe8fd8461f644291150b4c8da
yeonddori <yeonddori@gmail.com>
2025-03-30 23:30:09 +0900

    fix: 학습자료를 생성한 본인이 아니면 수정/삭제 버튼이 안 보이게 수정
    #406

--- src/components/Modal/types.ts
+++ src/components/Modal/types.ts
@@ -8,6 +8,7 @@ export interface ActionModalProps {
   mainButtonText: string;
   onMainButtonClick: () => void;
   size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl' | '6xl' | 'full';
+  isNoFooter?: boolean;
 }
 
 export interface AlertModalProps {
